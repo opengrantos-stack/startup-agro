6b0f6de387881e6ddac28213a0105de9c15c5dfa
Malaquias Calueio <Opengrant.os@gmail.com>
2026-08-18 11:22:28 +0100

    Add multichain Web3 orchestrator

--- web3/index.js
+++ web3/index.js
@@ -1,11 +1,51 @@
 const WEB3_CONFIG = require('./config');
 const WalletManager = require('./wallet');
+const EVMAdapter = require('./evm');
+const SolanaAdapter = require('./solana');
 const Registry = require('./registry');
 
 const wallet = new WalletManager(WEB3_CONFIG);
 
+function criarEVM(networkName) {
+    const network = WEB3_CONFIG.evm[networkName];
+
+    if (!network) {
+        throw new Error(`Rede EVM não encontrada: ${networkName}`);
+    }
+
+    return new EVMAdapter(network);
+}
+
+function criarSolana() {
+    return new SolanaAdapter(WEB3_CONFIG.solana);
+}
+
+function listarRedes() {
+    return {
+        evm: Object.entries(WEB3_CONFIG.evm)
+            .filter(([, network]) => network.enabled)
+            .map(([id, network]) => ({
+                id,
+                name: network.name,
+                chainId: network.chainId,
+                type: 'evm'
+            })),
+
+        solana: WEB3_CONFIG.solana.enabled
+            ? [{
+                id: 'solana',
+                name: WEB3_CONFIG.solana.name,
+                type: 'solana'
+            }]
+            : []
+    };
+}
+
 module.exports = {
     config: WEB3_CONFIG,
     wallet,
-    registry: Registry
+    registry: Registry,
+    criarEVM,
+    criarSolana,
+    listarRedes
 };
