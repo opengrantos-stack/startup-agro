04c61dfcaead14567bed403e41b7f1a48d1d57af
Malaquias Calueio <Opengrant.os@gmail.com>
2026-08-19 22:51:31 +0100

    Fix service worker cache for interface updates

--- sw.js
+++ sw.js
@@ -1,6 +1,39 @@
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open('agro-store').then((cache) => cache.addAll(['./', './index.html', './logo.png'])));
+const CACHE_NAME = 'agro-store-v2';
+
+self.addEventListener('install', (event) => {
+    self.skipWaiting();
+
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll([
+                './',
+                './index.html',
+                './logo.png'
+            ]);
+        })
+    );
 });
-self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then((response) => response || fetch(e.request)));
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((keys) => {
+            return Promise.all(
+                keys
+                    .filter((key) => key !== CACHE_NAME)
+                    .map((key) => caches.delete(key))
+            );
+        }).then(() => self.clients.claim())
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        fetch(event.request)
+            .then((response) => {
+                return response;
+            })
+            .catch(() => {
+                return caches.match(event.request);
+            })
+    );
 });
